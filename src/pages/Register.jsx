b56284a35c433e6e078ba32376206a748ab2c5db
Nirajan Karki <nirajankarki1985@gmail.com>
2022-10-17 05:30:03 +0200

    daily progress

---
+++ src/pages/Register.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://assets.zyrosite.com/cdn-cgi/image/format=auto,fit=crop/m5KxyljRNbc0W3jr/pbf06768-mk3neN7weyFpRVJ1.jpg")
+      center no-repeat;
+  background-size: cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  background-color: white;
+  width: 40%;
+`;
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 300;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: none;
+  background-color: teal;
+  color: white;
+  width: 40%;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE ACCOUNT</Title>
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
